ed3f176999eda9b009e2fb785ea6a7cda4739734
Sinchan <mr.sinchan.banerjee@gmail.com>
2022-09-07 14:26:51 -0400

    Update EtlDatawarehouseApplication.java
    fix for logger

--- Chapter 4/SpringBatchApp/EtlDatawarehouse/src/main/java/com/scalabledataarchitecture/etl/EtlDatawarehouseApplication.java
+++ Chapter 4/SpringBatchApp/EtlDatawarehouse/src/main/java/com/scalabledataarchitecture/etl/EtlDatawarehouseApplication.java
@@ -1,7 +1,7 @@
 package com.scalabledataarchitecture.etl;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
